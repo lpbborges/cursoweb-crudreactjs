dfc8678f4e2e4bf82932ab559b563198c6bf7a3b
lpbborges <luizpaulobborges@gmail.com>
2020-03-02 22:54:46 -0300

    Finish

---
+++ front-end/src/components/template/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/images/logo.png';
+
+import './Logo.css';
+
+export default props =>
+  <aside className="logo">
+    <Link to="/" className="logo">
+      <img src={logo} alt="logo"/>
+    </Link>
+  </aside>
